9d69c20d0025f1076007869ef3429a224b174c50
kevin <kelvinotix@gmail.com>
2023-02-28 12:20:08 -0800

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * _strlen - A functions that returns the length of a string.
- * @s: The string to get its length.
- *
- * Return: The length of @s
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-
 int _strlen(char *s)
 {
-	int length = 0;
+	int longi = 0;
 
-	while (s[length] != '\0')/*the array count != the last count of strings*/
+	while (*s != '\0')
 	{
-		length++;/*count length from zero*/
+		longi++;
+		s++;
 	}
 
-	return (length);
+	return (longi);
 }
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* This a header file containing declarations
- * for prototype functions defined in the 0x05-more-functions_nested_loops
- * tasks and directory
- * This is like creating my own library with declared functions
- * The tasks defines  the functions
- */
+#include <stdio.h>
+#include <unistd.h>
 
-/*enclosing the files  contentinbetween ifndef,define and endif,		      
-stops the compiler from processing the file twice if its included twice,
-if not so, this will cause an error*/
-
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -21,11 +13,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-int _putchar(char c);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-int _putchar(char c)
 
 #endif
 
